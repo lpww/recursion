9a503ab9b4f969abe451e555eafbee6480dbc638
lpww <t.treffry@gmail.com>
2015-01-07 00:51:55 -0800

    Refactor and comment stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,33 +4,40 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
+	//Store JSOn string
 	var result = '';
 
+	//Format for boolean base case
 	if(typeof obj === 'boolean'){
 		return obj.toString();
 	}
 
+	//Format for string base case
 	if(typeof obj === 'string'){
 		return '"' + obj + '"';
 	}
 
+	//Format for number base case
 	if(typeof obj === 'number'){
 		return obj.toString();
 	}
 
+	//Format for null base case
 	if(obj === null){
 		return 'null';
 	}
 
+	//Format for function and undefined base cases
+	if(typeof obj === 'function' || typeof obj === 'undefined'){
+		return undefined;
+	}
+
+	//Format array elements
 	if(obj.length !== undefined){
 		result = '[';
+
 		for(var i = 0; i < obj.length; i++){
-			if(typeof obj[i] === 'object'){
-				result += stringifyJSON(obj[i]);
-				if(i < obj.length - 1){
-					result += ',';
-				}
-			} else if(typeof obj[i] !== 'function' && typeof obj[i] !== 'undefined'){
+			if(typeof obj[i] !== 'function' && typeof obj[i] !== 'undefined'){
 				result += stringifyJSON(obj[i]);
 				if(i < obj.length - 1){
 					result += ',';
@@ -40,14 +47,13 @@ var stringifyJSON = function(obj) {
 		return result + ']';
 	}
 
+	//Format object elements
 	if(obj.length === undefined){
 		result = '{';
 
 		for(var key in obj){
 			if(obj.hasOwnProperty(key)){
-				if(typeof obj[key] === 'object'){
-					result += '"' + key + '":' + stringifyJSON(obj[key]) + ',';
-				} else if(typeof obj[key] !== 'function' && typeof obj[key] !== 'undefined'){
+				if(typeof obj[key] !== 'function' && typeof obj[key] !== 'undefined'){
 					result += '"' + key + '":' + stringifyJSON(obj[key]) + ',';
 				}
 			}
