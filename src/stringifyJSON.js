945c196199b88b6da66331da1421485a5c0fd18a
lpww <t.treffry@gmail.com>
2015-01-07 00:27:32 -0800

    Recursive solve JSON.stringify

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,60 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+	var result = '';
+
+	if(typeof obj === 'boolean'){
+		return obj.toString();
+	}
+
+	if(typeof obj === 'string'){
+		return '"' + obj + '"';
+	}
+
+	if(typeof obj === 'number'){
+		return obj.toString();
+	}
+
+	if(obj === null){
+		return 'null';
+	}
+
+	if(obj.length !== undefined){
+		result = '[';
+		for(var i = 0; i < obj.length; i++){
+			if(typeof obj[i] === 'object'){
+				result += stringifyJSON(obj[i]);
+				if(i < obj.length - 1){
+					result += ',';
+				}
+			} else if(typeof obj[i] !== 'function' && typeof obj[i] !== 'undefined'){
+				result += stringifyJSON(obj[i]);
+				if(i < obj.length - 1){
+					result += ',';
+				}
+			}
+		}
+		return result + ']';
+	}
+
+	if(obj.length === undefined){
+		result = '{';
+
+		for(var key in obj){
+			if(obj.hasOwnProperty(key)){
+				if(typeof obj[key] === 'object'){
+					result += '"' + key + '":' + stringifyJSON(obj[key]) + ',';
+				} else if(typeof obj[key] !== 'function' && typeof obj[key] !== 'undefined'){
+					result += '"' + key + '":' + stringifyJSON(obj[key]) + ',';
+				}
+			}
+		}
+
+		if(result.length > 1){
+			return result.slice(0,-1) + '}';
+		} else {
+			return result + '}';
+		}
+	 
+	}
 };
