910a62e7c39992e10102fb05a748efa2b15626ac
lpww <t.treffry@gmail.com>
2015-01-09 12:08:43 -0800

    Recursive solve for getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,10 +1,29 @@
-// If life was easy, we could just do things the easy way:
-// var getElementsByClassName = function (className) {
-//   return document.getElementsByClassName(className);
-// };
+getElementsByClassName = function(className, target){ 
+  //Store found elements
+  var result = [];
 
-// But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+  //If no target element is passed, search the entire document
+  if(target === undefined){
+    var searching = document.childNodes;
+  
+  } else {
+    var searching = target;
+  
+  }
+ 
+  //Loop through children
+  for(var i = 0; i < searching.length; i++){
+    //If classname is found, push the element to results array
+    if(searching[i].className !== undefined && searching[i].className.indexOf(className) > -1){
+      result.push(searching[i]);
+    
+    } 
+
+    //Can this function on any children of the element
+    result.push(getElementsByClassName(className, searching[i].childNodes));
+  
+  }
+  
+  //Result contains many nested arrays, use flatten to return single array with results
+  return _.flatten(result);
 };
